9fda2d69c6de066f6bd2f2cbf773183892b6ac94
pdurancastro <patryharuno@gmail.com>
2016-10-23 17:00:50 +0200

    Clienteej6

--- client.py
+++ client.py
@@ -9,19 +9,18 @@ import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 
-Lista = [sys.argv[1],sys.argv[2],sys.argv[3:]]
+Lista = [sys.argv[1],sys.argv[2],sys.argv[3:],sys.argv[3],sys.argv[4]]
 SERVIDOR = Lista[0]
 PUERTO = int(Lista[1])
-
-if len(Lista[2]) > 0:
-	LINEA = ' '.join(Lista[2])
-else:
-	sys.exit( 'No estas mandando texto al servidor')
- 
+LINEA = ' '.join(Lista[2])
+METODO = Lista[3]
+DIRECCION = Lista[4]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:    
     my_socket.connect((SERVIDOR, PUERTO))
+    if METODO == 'REGISTER':
+	    LINEA = METODO + " " + 'sip:' + " " + DIRECCION + " " + 'SIP/2.0\r\n\r\n'
     print("Enviando:", LINEA)
     my_socket.send(bytes(LINEA, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
