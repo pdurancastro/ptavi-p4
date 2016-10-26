1964433269cf3210337c3c9afe09d54c7b4409c0
Patricia Duran Castro <pcastro@alpha11.aulas.gsyc.urjc.es>
2016-10-26 14:58:04 +0200

    PEP8 en servidor

--- server.py
+++ server.py
@@ -14,63 +14,63 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    dict= {}
-    list_client= []     
-    
-    def handle(self):                            
-        client= self.rfile.read().decode('utf-8').split()             
-        #print(client)
-        if client[0] == "REGISTER":            
-            list_client=[self.client_address[0],client[4]]
+    dict = {}
+    list_client = []
+
+    def handle(self):
+        client = self.rfile.read().decode('utf-8').split()
+#       print(client)
+
+        if client[0] == "REGISTER":
+            list_client = [self.client_address[0], client[4]]
             self.dict[client[1]] = list_client
-            print( "(Cliente,Puerto)" + ":" + str(self.client_address)) #Imprime el cliente con su puerto
+#            Imprime el cliente con su puerto
+            print("(Cliente,Puerto)" + ":" + str(self.client_address))
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-        
-            if client[3] == "Expires:":                
+
+            if client[3] == "Expires:":
                 t = int(list_client[1])
                 tempo_cad = int(time.time() + t)
 
                 list_client[1] = tempo_cad
-                
+
                 print(list_client[1])
 
-                lista=[]       
+                lista = []
                 for cliente in self.dict:
                     tiempo_actual = int(time.time())
-                    if self.dict[cliente][1] < tiempo_actual:
+                    tiempo_exp = self.dict[cliente][1]
+                    if tiempo_exp < tiempo_actual:
                         lista.append(cliente)
                 for usuario in lista:
                     del self.dict[usuario]
-                
-                #for client in self.dict:
-                    #t = self.dict[client][1] 
-                    #client = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(t))
-                    #list_client[1]= client 
-                    #Errata si itero con el mismo cliente
-                
-                   
-            print(self.dict) #Imprimo el diccionario para ver si se borro la gente
+
+#                for client in self.dict:
+#                    t = self.dict[client][1]
+#                    client = time.strftime('%Y-%m-%d %H:%M:%S',
+#                                           time.gmtime(t))
+#                    list_client[1]= client
+#                    Errata si itero con el mismo cliente
+
+#           Imprimo el diccionario para ver si se borro la gente
+            print(self.dict)
             self.register2json()
             self.registered()
 
-            
-            
     def register2json(self):
-        json.dump(self.dict,open('registered.json', 'w'))
-        
+        json.dump(self.dict, open('registered.json', 'w'))
+
     def registered(self):
         try:
             with open('registered.json') as datafile:
                 self.dict = json.load(datafile)
         except:
-            print ('No hay registro')
-        
-
+            print('No hay registro')
 
 
 if __name__ == "__main__":
     PUERTO = int(sys.argv[1])
-    serv = socketserver.UDPServer(('', PUERTO),SIPRegisterHandler)
+    serv = socketserver.UDPServer(('', PUERTO), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
